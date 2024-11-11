7f19d042d713c349fb6369f1b26925a376ff5e54
Anchal kumari <anchalkum2630@gmail.com>
2024-11-11 14:35:54 +0530

    all code of booking

--- Server/index.js
+++ Server/index.js
@@ -31,7 +31,7 @@ const pool = mysql.createPool({
   // connectionLimit: 10,
   // queueLimit: 0
   host: process.env.DB_HOST,
-  port:3307,
+  port:3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME, // Replace with your DB name
